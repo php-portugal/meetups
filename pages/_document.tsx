3990e1f408a37c9c685ba22e36f5255a9a4e7d69
Wendell Adriel <wendelladriel.ti@gmail.com>
2022-11-15 12:40:27 +0000

    small improvements to UI and SEO (#8)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,8 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap"
           rel="stylesheet"
         />
+
+        <link rel="icon" href="https://fav.farm/🇵🇹" />
       </Head>
       <body>
         <Main />
