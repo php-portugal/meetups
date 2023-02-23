011153f1db427bc16669ee20cbe4f1387ab6efd5
Joao Patricio <ijpatricio@gmail.com>
2023-02-23 08:43:20 +0000

    Adds Gitpod support

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,35 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import {defineConfig} from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+
+require('dotenv').config()
+
+const extendedViteDevServerOptions = {}
+
+if (process.env.GITPOD_VITE_URL) {
+    extendedViteDevServerOptions.hmr = {
+        protocol: 'wss',
+        host: new URL(process.env.GITPOD_VITE_URL).hostname,
+        clientPort: 443
+    }
+}
 
 export default defineConfig({
+    server: {
+        ...extendedViteDevServerOptions
+    },
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
-});
+})
