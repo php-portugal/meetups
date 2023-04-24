de6ba5c173d7f7cc6a8c9d2161d93af3cf5050bc
André Varandas <andre.m.varandas@gmail.com>
2023-04-24 12:43:26 +0100

    fix redirect link to telegram

--- next.config.js
+++ next.config.js
@@ -4,16 +4,15 @@ const nextConfig = {
   swcMinify: true,
   async redirects() {
     return [
-      // TODO: Delete `pages/telegram.tsx` when redirect works
-      //
-      // {
-      //   source: '/telegram',
-      //   destination: 'https://t.me/+a1s1B-UyyhcxNWRk',
-      //   permanent: false,
-      // },
+      {
+        source: '/telegram',
+        destination: 'https://t.me/%2Ba1s1B-UyyhcxNWRk',
+        permanent: false,
+      },
       {
         source: '/leiria',
-        destination: 'https://ijpatricio.notion.site/Road-to-PHP-Portugal-Meetup-8-6c3bba4a566449d3adcbac79d6f485fe',
+        destination:
+          'https://ijpatricio.notion.site/Road-to-PHP-Portugal-Meetup-8-6c3bba4a566449d3adcbac79d6f485fe',
         permanent: false,
       },
       {
