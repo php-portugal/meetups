50e5a5faeb57249c1726aed2e8d52b45ae3edb9e
Joao Patricio <ijpatricio@gmail.com>
2023-04-24 12:10:52 +0100

    Meetup 8 Leiria

--- next.config.js
+++ next.config.js
@@ -4,11 +4,13 @@ const nextConfig = {
   swcMinify: true,
   async redirects() {
     return [
-      {
-        source: '/telegram',
-        destination: 'https://t.me/+a1s1B-UyyhcxNWRk',
-        permanent: false,
-      },
+      // TODO: Delete `pages/telegram.tsx` when redirect works
+      //
+      // {
+      //   source: '/telegram',
+      //   destination: 'https://t.me/+a1s1B-UyyhcxNWRk',
+      //   permanent: false,
+      // },
       {
         source: '/leiria',
         destination: 'https://ijpatricio.notion.site/Road-to-PHP-Portugal-Meetup-8-6c3bba4a566449d3adcbac79d6f485fe',
