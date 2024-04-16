641470f4be1e33f43f03ac4c34175a76c8791539
Nuno Maduro <enunomaduro@gmail.com>
2024-04-16 11:05:40 +0100

    Update call-for-speakers.blade.php

--- resources/views/call-for-speakers.blade.php
+++ resources/views/call-for-speakers.blade.php
@@ -1,18 +1,21 @@
 <x-layout>
     <div class="lg:min-h-[250px] flex flex-col items-center">
-        <p class="mt-20 text-xl">
-            Call for Speakers
-        </p>
-        <p class="mt-10">
-            Hey there! We’d be thrilled if you joined us as a speaker at PHP Portugal #12! We’ve already snagged one awesome speaker, <a href="https://twitter.com/dianaarnos" target="_blank" class="text-blue-600 font-bold">Diana Arnos</a>, but we’ve got room for another.
-        </p>
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <p class="mt-20 text-xl">
+                Call for Speakers
+            </p>
 
-        <p class="mt-10">
-            The talk slot duration is around 20 minutes, and it’s totally up to you how advanced the talk is! Trust me, people love even the more introductory talks. Anyway, drop your talk idea below, and I will let you know if you get selected.
-        </p>
+            <p class="mt-10">
+                Hey there! We’d be thrilled if you joined us as a speaker at PHP Portugal #12! We’ve already snagged one awesome speaker, <a href="https://twitter.com/dianaarnos" target="_blank" class="text-blue-600 font-bold">Diana Arnos</a>, but we’ve got room for another.
+            </p>
 
-        <p class="mt-10">
-            You can submit a talk proposal by filling out the form: <a href="https://forms.gle/ypPbHfcPuukE2uAX7" target="_blank" class="text-blue-600 font-bold">forms.gle/ypPbHfcPuukE2uAX7</a>.
-        </p>
+            <p class="mt-10">
+                The talk slot duration is around 20 minutes, and it’s totally up to you how advanced the talk is! Trust me, people love even the more introductory talks. Anyway, drop your talk idea below, and I will let you know if you get selected.
+            </p>
+
+            <p class="mt-10">
+                You can submit a talk proposal by filling out the form: <a href="https://forms.gle/ypPbHfcPuukE2uAX7" target="_blank" class="text-blue-600 font-bold">forms.gle/ypPbHfcPuukE2uAX7</a>.
+            </p>
+        </div>
     </div>
 </x-layout>
