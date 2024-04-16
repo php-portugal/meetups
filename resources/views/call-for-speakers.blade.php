bb26ba7d7c1b7f513ff9c608563a13f8e5268b63
Nuno Maduro <enunomaduro@gmail.com>
2024-04-16 11:04:19 +0100

    Updates call for speakers

--- resources/views/call-for-speakers.blade.php
+++ resources/views/call-for-speakers.blade.php
@@ -4,11 +4,15 @@
             Call for Speakers
         </p>
         <p class="mt-10">
-            Hey there! We’d be thrilled if you joined us as a speaker at PHP Portugal #12! We’ve already snagged one awesome speaker, <a href="https://twitter.com/dianaarnos" target="_blank" class="text-blue-600">Diana Arnos</a>, but we’ve got room for another.
+            Hey there! We’d be thrilled if you joined us as a speaker at PHP Portugal #12! We’ve already snagged one awesome speaker, <a href="https://twitter.com/dianaarnos" target="_blank" class="text-blue-600 font-bold">Diana Arnos</a>, but we’ve got room for another.
         </p>
 
         <p class="mt-10">
-            You can submit a talk proposal by filling out the form: <a href="https://forms.gle/ypPbHfcPuukE2uAX7" target="_blank" class="text-blue-600">forms.gle/ypPbHfcPuukE2uAX7</a>.
+            The talk slot duration is around 20 minutes, and it’s totally up to you how advanced the talk is! Trust me, people love even the more introductory talks. Anyway, drop your talk idea below, and I will let you know if you get selected.
+        </p>
+
+        <p class="mt-10">
+            You can submit a talk proposal by filling out the form: <a href="https://forms.gle/ypPbHfcPuukE2uAX7" target="_blank" class="text-blue-600 font-bold">forms.gle/ypPbHfcPuukE2uAX7</a>.
         </p>
     </div>
 </x-layout>
