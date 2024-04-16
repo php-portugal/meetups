d8d9fe4f42827691f040db5a9513e3ecae62ac5a
Nuno Maduro <enunomaduro@gmail.com>
2024-04-16 11:02:39 +0100

    Updates call for speakers

--- resources/views/call-for-speakers.blade.php
+++ resources/views/call-for-speakers.blade.php
@@ -4,7 +4,11 @@
             Call for Speakers
         </p>
         <p class="mt-10">
-            Coming soon
+            Hey there! We’d be thrilled if you joined us as a speaker at PHP Portugal #12! We’ve already snagged one awesome speaker, <a href="https://twitter.com/dianaarnos" target="_blank" class="text-blue-600">Diana Arnos</a>, but we’ve got room for another.
+        </p>
+
+        <p class="mt-10">
+            You can submit a talk proposal by filling out the form: <a href="https://forms.gle/ypPbHfcPuukE2uAX7" target="_blank" class="text-blue-600">forms.gle/ypPbHfcPuukE2uAX7</a>.
         </p>
     </div>
 </x-layout>
