161f3a7886e56723dea5013527aca1481773fcb7
Nuno Maduro <enunomaduro@gmail.com>
2024-05-01 12:37:31 +0100

    Update call-for-speakers.blade.php

--- resources/views/call-for-speakers.blade.php
+++ resources/views/call-for-speakers.blade.php
@@ -6,11 +6,7 @@
             </p>
 
             <p class="mt-10">
-                Hey there! We’d be thrilled if you joined us as a speaker at PHP Portugal #12! We’ve already snagged one awesome speaker, <a href="https://twitter.com/dianaarnos" target="_blank" class="text-blue-600 font-bold">Diana Arnos</a>, but we’ve got room for another.
-            </p>
-
-            <p class="mt-10">
-                The talk slot duration is around 20 minutes, and it’s totally up to you how advanced the talk is! Trust me, people love even the more introductory talks. Anyway, drop your talk idea below, and I will let you know if you get selected.
+                Hey there! We’d be thrilled if you joined us as a speaker at PHP Portugal #13! The talk slot duration is around 20 minutes, and it’s totally up to you how advanced the talk is! Trust me, people love even the more introductory talks. Anyway, drop your talk idea below, and I will let you know if you get selected.
             </p>
 
             <p class="mt-10">
